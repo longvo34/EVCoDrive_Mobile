0bf3b562f82137daea01297a1c78896c23b09933
longvo34 <vothanhlong0987@gmail.com>
2026-02-06 18:50:49 +0700

    fix register vehicle

--- src/services/station/station.service.js
+++ src/services/station/station.service.js
@@ -4,3 +4,7 @@ import api from "../api";
 export const getRecommendedStations = (userId) => {
   return api.get(`/stations/recommendations/${userId}`);
 };
+
+export const getStationById = (stationId) => {
+  return api.get(`/stations/${stationId}`);
+};
